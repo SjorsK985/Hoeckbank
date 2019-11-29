6e2db76259d1e9ba9b58f8967e140da4daa5fe05
Martijn de Haar <martijn.de.haar@hva.nl>
2019-11-29 13:52:08 +0100

    project startup

---
+++ Hoeckbank/src/main/java/hoeckbankgroup/demo/HoeckbankApplication.java
@@ -0,0 +1,13 @@
+package hoeckbankgroup.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoeckbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoeckbankApplication.class, args);
+    }
+
+}
---
+++ Hoeckbank/src/test/java/hoeckbankgroup/demo/HoeckbankApplicationTests.java
@@ -0,0 +1,13 @@
+package hoeckbankgroup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoeckbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
