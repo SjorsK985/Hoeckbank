1d12b3fe09689a6ac907cfa70e6e3df278c3838f
Roeland Kulk <roelandkulk@gmail.com>
2019-12-05 16:03:31 +0100

    AddTestKlantToDb.java toegevoegd

--- Hoeckbank/src/main/java/hoeckbankgroup/demo/controller/AddTestKlantToDb.java
+++ Hoeckbank/src/main/java/hoeckbankgroup/demo/controller/AddTestKlantToDb.java
@@ -1,7 +1,7 @@
 package hoeckbankgroup.demo.controller;
 
-import hoeckbankgroup.demo.model.Klant;
 import hoeckbankgroup.demo.model.KlantDAO;
+import hoeckbankgroup.demo.model.TestKlant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +14,8 @@ public class AddTestKlantToDb {
 
     @GetMapping("fdb")
     public String fillDatabase(){
+        TestKlant testKlant = new TestKlant("user", "test");
+        klantDAO.save(testKlant);
 
         return "login";
     }
