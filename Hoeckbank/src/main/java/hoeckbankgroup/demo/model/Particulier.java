2b059aa0a4e87602a66b24b4a3893972bd58d2f0
Anne vd Bosch <vdbosch.am@gmail.com>
2019-12-10 16:53:06 +0100

    particulier localdate aangepast

--- Hoeckbank/src/main/java/hoeckbankgroup/demo/model/Particulier.java
+++ Hoeckbank/src/main/java/hoeckbankgroup/demo/model/Particulier.java
@@ -14,7 +14,7 @@ public class Particulier extends Klant {
     private int BSN;
 
     private String geslacht;
-    private String geboorteDatum;
+    private LocalDate geboorteDatum;
 
 
     public Particulier(){
@@ -25,7 +25,7 @@ public class Particulier extends Klant {
         super(email, wachtwoord, straat, huisnummer, postcode, woonplaats, telefoonNummer, rekeningen);
     }*/
 
-    public Particulier(String email, String wachtwoord, String straat, String huisnummer, String postcode, String woonplaats, String telefoonNummer, List<Rekening> rekeningen, String voornaam, String tussenvoegsel, String achternaam, int BSN, String geslacht, String geboorteDatum) {
+    public Particulier(String email, String wachtwoord, String straat, String huisnummer, String postcode, String woonplaats, String telefoonNummer, List<Rekening> rekeningen, String voornaam, String tussenvoegsel, String achternaam, int BSN, String geslacht, LocalDate geboorteDatum) {
         super(email, wachtwoord, straat, huisnummer, postcode, woonplaats, telefoonNummer, rekeningen);
         this.voornaam = voornaam;
         this.tussenvoegsel = tussenvoegsel;
@@ -85,11 +85,11 @@ public class Particulier extends Klant {
         this.geslacht = geslacht;
     }
 
-    public String getGeboorteDatum() {
+    public LocalDate getGeboorteDatum() {
         return geboorteDatum;
     }
 
-    public void setGeboorteDatum(String geboorteDatum) {
+    public void setGeboorteDatum(LocalDate geboorteDatum) {
         this.geboorteDatum = geboorteDatum;
     }
 
