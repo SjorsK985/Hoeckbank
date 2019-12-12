d32a5c71f2d7afb100d6afa04f5fa06029d301ab
Roeland Kulk <roelandkulk@gmail.com>
2019-12-12 14:37:32 +0100

    rekeningdetail werkt behalve transacties

--- Hoeckbank/src/main/java/hoeckbankgroup/demo/controller/rekeningDetailController.java
+++ Hoeckbank/src/main/java/hoeckbankgroup/demo/controller/rekeningDetailController.java
@@ -1,15 +1,24 @@
 package hoeckbankgroup.demo.controller;
 
+import hoeckbankgroup.demo.model.Rekening;
+import hoeckbankgroup.demo.model.service.RekeningService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class rekeningDetailController {
 
+    @Autowired
+    private RekeningService rekeningService;
+
     @GetMapping("rekeningdetail")
-    public String rekeningDetailHandler(@RequestParam int id){
-        System.out.println(id);
+    public String rekeningDetailHandler(@RequestParam int id, Model model){
+        Rekening rekening = rekeningService.findRekeningByRekeningID(id);
+        model.addAttribute("rekening", rekening);
         return "rekeningdetail";
     }
 }
