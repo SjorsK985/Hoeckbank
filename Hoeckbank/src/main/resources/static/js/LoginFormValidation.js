6b059266e34e2ad5c656f9ea33e3dbcb5207c2c5
Roeland Kulk <roelandkulk@gmail.com>
2019-12-12 11:10:16 +0100

    master commitje

--- Hoeckbank/src/main/resources/static/js/LoginFormValidation.js
+++ Hoeckbank/src/main/resources/static/js/LoginFormValidation.js
@@ -1,11 +1,11 @@
 function validationError(){
-    window.alert("sometext");
+
     var userName = document.loginForm.gebruiker_naam.value;
     var password = document.loginForm.gebruiker_paswoord.value;
 
     //RK: reset error berichten voor het geval de gebruik al errors heeft
-    document.getElementById("passwordError").innerHTML = "";
-    document.getElementById("userNameError").innerHTML = "";
+    document.getElementById("passwordError").innerHTML = "&nbsp";
+    document.getElementById("userNameError").innerHTML = "&nbsp";
 
     if(password === "" && userName === ""){
         document.getElementById("userNameError").innerHTML = "Gebruikersnaam mag niet leeg zijn";
