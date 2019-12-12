8ff3cceb6732d706d55ab8747aa6fda4b555a422
Roeland Kulk <roelandkulk@gmail.com>
2019-12-12 13:48:36 +0100

    rekening id werkt

--- Hoeckbank/src/main/java/hoeckbankgroup/demo/controller/rekeningDetailController.java
+++ Hoeckbank/src/main/java/hoeckbankgroup/demo/controller/rekeningDetailController.java
@@ -2,12 +2,14 @@ package hoeckbankgroup.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class rekeningDetailController {
 
     @GetMapping("rekeningdetail")
-    public String rekeningDetailHandler(){
+    public String rekeningDetailHandler(@RequestParam int id){
+        System.out.println(id);
         return "rekeningdetail";
     }
 }
