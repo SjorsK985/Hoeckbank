8b54b2bc719411d94a2865cc7d6b4baffba49555
Ruben van den Akker <ruben.van.den.akker@hva.nl>
2019-12-12 15:33:04 +0100

    loguit front is af, backend moet het nog werkend gemaakt worden

--- Hoeckbank/src/main/java/hoeckbankgroup/demo/controller/LogUitController.java
+++ Hoeckbank/src/main/java/hoeckbankgroup/demo/controller/LogUitController.java
@@ -4,13 +4,18 @@ import hoeckbankgroup.demo.model.Gebruiker;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
+
 public class LogUitController {
 
-    @GetMapping("loguit")
-    public String logUitHandler(Model model){
-
+    @PostMapping("loguit")
+    public String logUitHandler(){
         return "index";
     }
 
