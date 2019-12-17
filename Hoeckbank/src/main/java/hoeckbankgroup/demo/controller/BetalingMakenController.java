073bf8d2639388d09e5617eefd2f485e3e840d62
Roeland Kulk <roelandkulk@gmail.com>
2019-12-17 16:51:16 +0100

    Geld overmaken werkt, nog paar kleine bugjes

--- Hoeckbank/src/main/java/hoeckbankgroup/demo/controller/BetalingMakenController.java
+++ Hoeckbank/src/main/java/hoeckbankgroup/demo/controller/BetalingMakenController.java
@@ -22,13 +22,20 @@ public class BetalingMakenController {
     private TransactieService transactieService;
 
     @GetMapping("betalingmaken")
-    public String betalingMakenHandler(@RequestParam int id, Model model){
+    public String betalingMakenHandler(@RequestParam int id,
+                                       @RequestParam (value = "melding", required = false) String melding,
+                                       Model model){
         Rekening rekening = rekeningService.findRekeningByRekeningID(id);
         model.addAttribute("rekening", rekening);
+        model.addAttribute("error", melding);
         //RK: Voor testdoeleinden:
         //rekeningService.setTestSaldo(id, 2000.00);
         return "betalingmaken";
     }
+    //Todo: Refactor
+    //Todo: Los data probleem op met none unique rekening
+    //Todo: Front-end validatie
+    //Todo:
 
     @PostMapping("do_transactie")
     public String doTransactieHandler(@RequestParam (name="rekening_id") int rekeningId,
@@ -37,7 +44,7 @@ public class BetalingMakenController {
                                       @RequestParam (name="rekeningnummer_ontvanger") String rekeningnummerOntvanger,
                                       @RequestParam String omschrijving,
                                       Model model){
-
+        System.out.println("RekeningID = " + rekeningId);
         //TODO: Controleer of bedrag niet negatief, 0 of groter dan saldo is
         //TODO: Check of tegenrekening bestaan (en naam overeenkomt)
         Rekening rekening = rekeningService.findRekeningByRekeningID(rekeningId);
@@ -45,8 +52,7 @@ public class BetalingMakenController {
         String validatieError = valideerTransactie(bedrag, rekening, tegenRekening);
         if(!validatieError.equals("")){
             model.addAttribute("error", validatieError);
-            System.out.println(validatieError);
-            return "redirect:/betalingmaken?id=" + rekeningId;
+            return "redirect:/betalingmaken?id=" + rekeningId + "&melding=" +validatieError;
         }
         //TODO: Maak object aan transactie aan
         Transactie transactie = new Transactie(rekeningnummerOntvanger, -bedrag, omschrijving, LocalDate.now());
@@ -60,6 +66,7 @@ public class BetalingMakenController {
         //TODO: Pas saldo eigen rekening  & tegenrekening aan
         saldosAanpassen(rekening, tegenRekening, bedrag);
         return "redirect:/rekeningdetail?id=" + rekeningId;
+
     }
 
     public String valideerTransactie(double bedrag, Rekening rekening, Rekening tegenRekening){
@@ -89,6 +96,5 @@ public class BetalingMakenController {
         rekening.setSaldo(nieuwSaldo);
         rekeningService.save(rekening);
     }
-
 
 }
