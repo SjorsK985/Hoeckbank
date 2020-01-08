1a7cb4afba55fcd17ce4aee185c63c046842541b
Roeland Kulk <roelandkulk@gmail.com>
2020-01-08 15:20:05 +0100

    Refactor RekeningDetailController done

--- Hoeckbank/src/main/java/hoeckbankgroup/demo/model/service/TransactieService.java
+++ Hoeckbank/src/main/java/hoeckbankgroup/demo/model/service/TransactieService.java
@@ -6,6 +6,9 @@ import hoeckbankgroup.demo.model.Transactie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class TransactieService {
@@ -43,5 +46,16 @@ public class TransactieService {
         //Pas saldo aan
         rekening.setSaldo(bedrag + rekening.getSaldo());
         rekeningService.save(rekening);
+    }
+
+    public ArrayList<Transactie> getLastTransactions(Rekening rekening){
+        List<Transactie> alleTransacties = rekening.getTransactiehistorie();
+        Collections.sort(alleTransacties);
+        // Zet transacties in nieuwe lijst om er max 10 te weergeven:
+        ArrayList<Transactie> transacties = new ArrayList<>();
+        for (int i = 0; i < 10 && i < alleTransacties.size(); i++) {
+            transacties.add(alleTransacties.get(i));
+        }
+        return transacties;
     }
 }
